67acd4e16449e1ccb3df71b45e9992cbfebcbbb9
Vadim Yaremchuk <mastinno@gmail.com>
2016-08-09 21:38:50 -0700

    fix broken build

--- src/main/java/org/geojson/jackson/LngLatAltSerializer.java
+++ src/main/java/org/geojson/jackson/LngLatAltSerializer.java
@@ -15,10 +15,13 @@ public class LngLatAltSerializer extends JsonSerializer<LngLatAlt> {
 	/**
 	 * The following must convert double to String in a much more efficient way then Double.toString()
 	 *
-	 * @See http://stackoverflow.com/questions/10553710/fast-double-to-string-conversion-with-given-precision
-	 * @param val
-	 * @param precision
-	 * @return
+	 * Link: http://stackoverflow.com/questions/10553710/fast-double-to-string-conversion-with-given-precision
+	 * 
+	 * TODO: there is no need in this function anymore
+	 * 
+	 * @param val double value that need to be serialized 
+	 * @param precision precision for serialization
+	 * @return serialized double
 	 */
 	protected static String fastDoubleToString(double val, int precision) {
 		StringBuilder sb = new StringBuilder();
@@ -51,7 +54,6 @@ public class LngLatAltSerializer extends JsonSerializer<LngLatAlt> {
 		jgen.writeNumber(value.getLatitude());
 		if (value.hasAltitude()) {
 			jgen.writeNumber(value.getAltitude());
-
 			for(double d : value.getAdditionalElements()) {
 				jgen.writeNumber(d);
 			}
